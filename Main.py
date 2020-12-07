e7fb6776e41c877af0fdbba108f09f40fe91f1c8
xIKYx <75186319+xIKYx@users.noreply.github.com>
2020-12-07 15:05:44 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,4 +31,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('NzgzMTU5NjQzMTI2ODkwNTE3.X8WsQQ.goE7tvhANk26UXXQ76pLkYm3Y2Y')
+client.run('my_token_here')
