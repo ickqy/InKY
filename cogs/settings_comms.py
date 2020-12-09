a18c91ff15a8972267843b58d6caf159109fc1aa
Fernando Duarte <75186319+xIKYx@users.noreply.github.com>
2020-12-09 10:32:22 -0600

    fixed an error

--- cogs/settings_comms.py
+++ cogs/settings_comms.py
@@ -16,15 +16,15 @@ class Settings(commands.Cog):
     # Commands
     @commands.command()
     async def ping(self, ctx, arg):
-            if arg == "pong":
-                return await ctx.send("Congratulation, you just ponged yourself lol")
+        if arg == "pong":
+            return await ctx.send("Congratulation, you just ponged yourself lol")
 
-            else:
-                start = time.perf_counter()
-                message = await ctx.send("Ping...")
-                end = time.perf_counter()
-                duration = (end - start) * 1000
-                await message.edit(content='Pong! {:.2f}ms'.format(duration))
+        else:
+            start = time.perf_counter()
+            message = await ctx.send("Ping...")
+            end = time.perf_counter()
+            duration = (end - start) * 1000
+            await message.edit(content='Pong! {:.2f}ms'.format(duration))
 
     @commands.command(aliases=["bi", "about", "info",])
     async def botinfo(self, ctx):
