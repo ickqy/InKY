77d16d833157d98f7ccace48f64fed2b1befe06e
Fernando Duarte <75186319+xIKYx@users.noreply.github.com>
2020-12-10 09:09:58 -0600

    add \a fun lil system

--- cogs/fun_comms.py
+++ cogs/fun_comms.py
@@ -6,6 +6,7 @@ class Fun(commands.Cog):
 
     def __innit__(self, client):
         self.client = client
+        self.pins = []
 
     # Commands
     @commands.command()
@@ -175,6 +176,37 @@ class Fun(commands.Cog):
             except UnboundLocalError:
                 pass
 
+    @commands.Cog.listener()
+    async def on_reaction_add(self, ctx, reaction, user):
+        if (
+            reaction.emoji == "⭐"
+            and not reaction.message.id in self.pins
+            and reaction.count >= 5
+        ):
+            self.pins.append(reaction.message.id)
+
+            embed = discord.Embed(
+                title="**New Starred Message**",
+                description=reaction.message.content,
+                colour=discord.Colour(0xB92C36),
+                url=reaction.message.jump_url,
+                timestamp=reaction.message.created_at,
+            )
+
+            for attachement in reaction.message.attachments:
+                if attachement.height:
+                    embed.set_image(url=attachement.url)
+            embed.set_author(
+                name=str(reaction.message.author),
+                icon_url=reaction.message.author.avatar_url_as(format="png"),
+            )
+            embed.set_footer(text=reaction.message.id)
+
+            # embed.add_field(name="Stars", value=reaction.count)
+            channel = self.client.get_channel(
+                int(self.client.config[str(reaction.message.guild.id)]["pins_channel"])
+            )
+            await channel.send(embed=embed)
 
 def setup(client):
     client.add_cog(Fun(client))
