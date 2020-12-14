c7df295f8b575e82b3c50f00319f4b0b29e54025
Fernando Duarte <75186319+xIKYx@users.noreply.github.com>
2020-12-13 21:15:35 -0600

    added some moderarion commands

---
+++ cogs/mod_comms.py
@@ -0,0 +1,45 @@
+import discord
+import random
+from discord.ext import commands
+
+class Moderation(commands.Cog):
+
+    def __innit__(self, client):
+        self.client = client
+
+    @commands.command(aliases=["cc"])
+    @commands.has_permissions(manage_messages = True)
+    async def purge(self, ctx, amount=1):
+        await ctx.channel.purge(limit=amount)
+
+    @commands.command()
+    @commands.has_permissions(kick_members = True)
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        await member.send(f"You Have been kicked from{ctx.guild.name} for {reason}!")
+        await ctx.send(f'{member.mention} has been kicked from the server')
+        await member.kick(reason=reason)
+
+    @commands.command()
+    @commands.has_permissions(ban_members = True)
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await member.send(f"You Have been Banned from{ctx.guild.name} for {reason}!")
+        await ctx.send(f'{member.mention} has been Banned from the server')
+        await member.ban(reason=reason)
+
+    @commands.command
+    @commands.has_permissions(ban_members = True)
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned {user.mention}')
+                await member.send(f"You Have been Unbanned from{ctx.guild.name}!")
+                return
+
+def setup(client):
+    client.add_cog(Moderation(client))
