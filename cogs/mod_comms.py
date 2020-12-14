076858590603a12018e5a3dd64baf808e429fd3f
Fernando Duarte <75186319+xIKYx@users.noreply.github.com>
2020-12-14 10:02:49 -0600

    made some changes to mod commands

--- cogs/mod_comms.py
+++ cogs/mod_comms.py
@@ -10,12 +10,14 @@ class Moderation(commands.Cog):
     @commands.command(aliases=["cc"])
     @commands.has_permissions(manage_messages = True)
     async def purge(self, ctx, amount=1):
+        """- Delete messages in bulck"""
         await ctx.channel.purge(limit=amount)
         await ctx.send("Messages Deleted", delete_after=5)
 
     @commands.command()
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
+        """- Kick a Member"""
         await member.send(f"You Have been kicked from {ctx.guild.name} for {reason}!")
         await ctx.send(f'{member.mention} has been kicked from the server')
         await member.kick(reason=reason)
@@ -23,13 +25,15 @@ class Moderation(commands.Cog):
     @commands.command()
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
+        """- Ban a Member"""
         await member.send(f"You Have been Banned from {ctx.guild.name} for {reason}!")
         await ctx.send(f'{member.mention} has been Banned from the server')
         await member.ban(reason=reason)
 
-    @commands.command
+    @commands.command()
     @commands.has_permissions(ban_members = True)
     async def unban(self, ctx, *, member):
+        """- Unban a Member"""
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split('#')
 
@@ -39,6 +43,7 @@ class Moderation(commands.Cog):
             if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
                 await ctx.send(f'Unbanned {user.mention}')
+                await member.send(f"You Have been Unbanned from {ctx.guild.name}!")
                 return
 
 def setup(client):
