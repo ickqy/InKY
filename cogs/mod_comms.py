b7be4301719c498a796ba99a64a2d86f186ef681
Fernando Duarte <75186319+xIKYx@users.noreply.github.com>
2020-12-14 09:56:19 -0600

    made some changes

--- cogs/mod_comms.py
+++ cogs/mod_comms.py
@@ -11,18 +11,19 @@ class Moderation(commands.Cog):
     @commands.has_permissions(manage_messages = True)
     async def purge(self, ctx, amount=1):
         await ctx.channel.purge(limit=amount)
+        await ctx.send("Messages Deleted", delete_after=5)
 
     @commands.command()
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
-        await member.send(f"You Have been kicked from{ctx.guild.name} for {reason}!")
+        await member.send(f"You Have been kicked from {ctx.guild.name} for {reason}!")
         await ctx.send(f'{member.mention} has been kicked from the server')
         await member.kick(reason=reason)
 
     @commands.command()
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
-        await member.send(f"You Have been Banned from{ctx.guild.name} for {reason}!")
+        await member.send(f"You Have been Banned from {ctx.guild.name} for {reason}!")
         await ctx.send(f'{member.mention} has been Banned from the server')
         await member.ban(reason=reason)
 
@@ -38,7 +39,6 @@ class Moderation(commands.Cog):
             if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
                 await ctx.send(f'Unbanned {user.mention}')
-                await member.send(f"You Have been Unbanned from{ctx.guild.name}!")
                 return
 
 def setup(client):
